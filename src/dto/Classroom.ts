21802b06ff3ad1f6969e1b1b9ceb45c433eb92a3
Michelangelo Camaioni <42435587+Genio2003@users.noreply.github.com>
2023-07-26 18:04:24 +0200

    WIP
    Co-authored-by: Manuel Doneda <Melagranata@users.noreply.github.com>

---
+++ src/dto/Classroom.ts
@@ -0,0 +1,5 @@
+export type Classroom = {
+    id: number,
+    name: string,
+    color: string;
+}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,56 @@
+import axios from "axios";
+import { apiUrl } from "../config";
+import { TokenContext } from "../context/TokenContext";
+import { useContext, useEffect, useState } from "react";
+import { Event } from "../dto/Event";
+import { CourseContext } from "../context/CourseContext";
 export function Home() {
+
+    const { token } = useContext(TokenContext);
+    const { course } = useContext(CourseContext);
+
+    const [events, setEvents] = useState<Event[]>([]);
+
+    useEffect(() => {
+        axios.get(new URL(`event/${encodeURIComponent(course?.id as number)}`, apiUrl).toString(), {
+            headers: {
+                Authorization: "Bearer " + token
+            },
+            params: {
+                start: '2023-01-20 07:0:0',
+                end: '2023-01-21 0:0:0'
+            }
+        }).then(response => { 
+            let result: Event[] = [];
+
+            (response.data as any[]).forEach(element => {
+                result.push(JSON.parse(element));
+            });
+            
+            setEvents(result);
+        });
+    }, [token]);
+
+    const SUS = () => {
+        if (events.length > 0) {
+            return events.map((event) => {
+                return (
+                    <div key={event.id}>
+                        <p>{event.subject}</p>
+                        <p>Aula {event.classroom.name}</p>
+                    </div>
+                )
+            });
+        } else {
+            return (<p>NO Event!</p>)
+        }
+    }
+
     return (
         <>
             <h1>Home</h1>
+            { typeof events[0]?.start}
+            { SUS() }
         </>
     );
 }
