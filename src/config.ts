0d9e7c219f2ca3ec01f31db3a5f5d61cfd76ac5e
Manuel Doneda <manuel.doneda@gmail.com>
2023-07-26 11:51:37 +0200

    fix: :adhesive_bandage: Set environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,5 @@
-export const apiUrl = new URL("https://calendar.zucchina.org/dev/");
+if (typeof import.meta.env.VITE_API_URL !== "string") {
+    console.error("VITE_API_URL environment variable is not set");
+}
+
+export const apiUrl = new URL(import.meta.env.VITE_API_URL);
