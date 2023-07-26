218ff1d8d976e1cd8f3f1c976454df5ef547ec43
Michelangelo Camaioni <42435587+Genio2003@users.noreply.github.com>
2023-07-26 20:26:51 +0200

    Implemented time logic

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -12,13 +12,17 @@ export function Home() {
     const [events, setEvents] = useState<Event[]>([]);
 
     useEffect(() => {
+        const start = new Date().toISOString();
+        const tomorrow = new Date(new Date().setDate(new Date().getDate() + 1));
+        const end = tomorrow.getFullYear() + "-" + (tomorrow.getMonth() + 1) + "-" + tomorrow.getDate() + "T00:00:00.000Z";
+
         axios.get(new URL(`event/${encodeURIComponent(course?.id as number)}`, apiUrl).toString(), {
             headers: {
                 Authorization: "Bearer " + token
             },
             params: {
-                start: '2023-01-20 0:0:0',
-                end: '2023-01-21 0:0:0'
+                start: start,
+                end: end
             }
         }).then(response => { 
             let result: Event[] = [];
@@ -35,28 +39,27 @@ export function Home() {
         });
     }, [token]);
 
-    const SUS = () => {
+    const dayEvents = () => {
         if (events.length > 0) {
             return events.map((event) => {
                 return (
                     <div key={event.id}>
                         <p>{event.subject}</p>
-                        <p>{event.start.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })} - {event.end.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</p>
+                        <p>{event.start.toLocaleTimeString([], { day: "2-digit", month: "2-digit", hour: "2-digit", minute: "2-digit" })} - {event.end.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</p>
                         <p>Aula {event.classroom.name}</p>
                     </div>
                 )
             });
         } else {
-            return (<p>NO Event!</p>)
+            return (<p>NESSUNA Lezione!</p>)
         }
     }
 
     return (
         <>
-            <h1>Home</h1>
             <h2>{course?.code}</h2>
             <h3>{course?.name}</h3>
-            { SUS() }
+            { dayEvents() }
         </>
     );
 }
