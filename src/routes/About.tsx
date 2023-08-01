5f71c641a4b925b09d3d55e806104fc5d1734dfb
Michelangelo Camaioni <42435587+Genio2003@users.noreply.github.com>
2023-08-01 13:10:01 +0200

    Added some questions...

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -2,7 +2,22 @@ export function About() {
     return (
         <>
             <h1>About</h1>
-            <p>Project Betoniera non è sponsorizzato e/o approvato da Fondazione JobsAcademy.</p>
+            <br></br>
+            
+            <h2>Project Betoniera? Che roba è??</h2>
+            <p>Boh...</p>
+
+            <h2>Chi ha fatto questo sito?</h2>
+            <p>Boh...</p>
+
+            <h2>Perché?</h2>
+            <p>Boh...</p>
+
+            <h2>Perché il nome Betoniera?</h2>
+            <p>Boh...</p>
+
+            <h2>Come funziona?</h2>
+            <p>Boh...</p>
         </>
     );
 }
