942c7c4b89114615f36bee5aafc772b6d6a64523
Michelangelo Camaioni <42435587+Genio2003@users.noreply.github.com>
2023-08-02 19:34:53 +0200

    Funny 404 GIFs?

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,9 +1,20 @@
-import "../assets/logo.svg";
-
 export function NotFound() {
+
+    function randomImage() {
+
+        const images = ["cement_mixer.gif", "eddsworld_cement.gif", "spinning_1.gif", "spinning_2.gif", "what_cement_mixer_are_you.gif"]
+        const num = Math.floor(Math.random() * images.length);
+
+        return (
+            <>
+                <img src={images[num]} alt="logo" />
+            </>
+        )
+    }
+
     return (
         <div className="container">
-            <img src="logo.svg" alt="logo" />
+            {randomImage()}
             <h1>404 Not Found</h1>
             <p>The page you are looking for does not exist.</p>
         </div>
