9b079867adcc54b9bec959c9080b7e19dbedffb6
Manuel Doneda <manuel.doneda@gmail.com>
2023-08-04 22:21:16 +0200

    style: :lipstick: Update about page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,20 +1,24 @@
 export function About() {
     return (
-        <>
-            <h1>About</h1>
-            <br></br>
+        <div className="container align-left">
+            <div className="container align-left wide">
+                <h1>About</h1>
+                <br></br>
+            </div>
 
-            <h2>Project Betoniera? Che roba è??</h2>
-            <p>Project Betoniera è un frontend alternativo al <a href="https://gestionale.fondazionejobsacademy.org" target="_blank">Gestionale della JAC</a> che permette in oltre di visualizzare lo stato della aule (libera/occupata) e di avere il calendario delle lezioni sulla tua app preferita! (<b>Google Calendar/Calendario di Apple/Outlook</b>)</p>
+            <div className="container align-left">
+                <h2>Project Betoniera? Che roba è??</h2>
+                <p>Project Betoniera è un frontend alternativo al <a href="https://gestionale.fondazionejobsacademy.org" target="_blank">Gestionale della JAC</a> che permette in oltre di visualizzare lo stato della aule (libera/occupata) e di avere il calendario delle lezioni sulla tua app preferita! (<b>Google Calendar/Calendario di Apple/Outlook</b>)</p>
 
-            <h2>Perché?</h2>
-            <p>Il Gestionale attuale è macchinoso e lento! 🥱 Sopratutto se lo si prova ad utilizzare da smartphone metre si è in ritardo per una lezione...😅</p>
+                <h2>Perché?</h2>
+                <p>Il Gestionale attuale è macchinoso e lento! 🥱 Sopratutto se lo si prova ad utilizzare da smartphone metre si è in ritardo per una lezione...😅</p>
 
-            <h2>Betoniera? Come mai questo nome? 🧐</h2>
-            <p>Il nome Betoniera deriva dal fatto che estraiamo i dati dal gestionale e li rimescoliamo in modo da renderli utilizzabili! Come una betoniera!</p>
+                <h2>Betoniera? Come mai questo nome? 🧐</h2>
+                <p>Il nome Betoniera deriva dal fatto che estraiamo i dati dal gestionale e li rimescoliamo in modo da renderli utilizzabili! Come una betoniera!</p>
 
-            <h2>Come funziona?</h2>
-            <p>Estraiamo i dati del calendario dal Gestionale ufficiale, li parsiamo, li salviamo in una cache e li serviamo pronti per essere utilizzati!</p>
-        </>
+                <h2>Come funziona?</h2>
+                <p>Estraiamo i dati del calendario dal Gestionale ufficiale, li parsiamo, li salviamo in una cache e li serviamo pronti per essere utilizzati!</p>
+            </div>
+        </div>
     );
 }
