886040652186b3bacd44c61048ea92fd48cfc9bc
Michelangelo Camaioni <42435587+Genio2003@users.noreply.github.com>
2023-08-14 18:52:26 +0200

    Removed Emoji from About Page...

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -11,9 +11,9 @@ export function About() {
                 <p>Project Betoniera è un frontend alternativo al <a href="https://gestionale.fondazionejobsacademy.org" target="_blank">Gestionale della JAC</a> che permette in oltre di visualizzare lo stato della aule (libera/occupata) e di avere il calendario delle lezioni sulla tua app preferita! (<b>Google Calendar/Calendario di Apple/Outlook</b>)</p>
 
                 <h2>Perché?</h2>
-                <p>Il Gestionale attuale è macchinoso e lento! 🥱 Sopratutto se lo si prova ad utilizzare da smartphone metre si è in ritardo per una lezione...😅</p>
+                <p>Il Gestionale attuale è macchinoso e lento! Sopratutto se lo si prova ad utilizzare da smartphone mentre si è in ritardo per una lezione...</p>
 
-                <h2>Betoniera? Come mai questo nome? 🧐</h2>
+                <h2>Betoniera? Come mai questo nome?</h2>
                 <p>Il nome Betoniera deriva dal fatto che estraiamo i dati dal gestionale e li rimescoliamo in modo da renderli utilizzabili! Come una betoniera!</p>
 
                 <h2>Come funziona?</h2>
