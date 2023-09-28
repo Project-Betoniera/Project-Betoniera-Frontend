4ff5d76b99214166f53dfeda61d775537da81d03
Manuel Doneda <manuel.doneda@gmail.com>
2023-09-28 12:27:48 +0200

    feat: :lipstick: Update navbar to fluentUI

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -1,11 +1,37 @@
 import { useContext } from "react";
 import { Outlet } from "react-router";
-import { Link } from "react-router-dom";
 import { TokenContext } from "./context/TokenContext";
 import { CourseContext } from "./context/CourseContext";
-import { Button } from "@fluentui/react-components";
+import { Button, CompoundButton, Label, Link, makeStyles, shorthands, tokens } from "@fluentui/react-components";
+import { Home20Filled, ConferenceRoom20Filled, Calendar20Filled, Info20Filled } from "@fluentui/react-icons";
+import { useGlobalStyles } from "./globalStyle";
+
+const useStyles = makeStyles({
+    header: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        ...shorthands.margin("1rem"),
+        ...shorthands.padding("0.5rem", "1rem"),
+        ...shorthands.borderRadius("1rem"),
+        backgroundColor: tokens.colorNeutralBackground2
+    },
+    nav: {
+        display: "flex",
+        flexGrow: "1",
+        justifyContent: "space-around",
+        alignItems: "center",
+    },
+    title: {
+        ...shorthands.margin("0"),
+        ...shorthands.padding("0"),
+    }
+});
+
 
 export function Wrapper() {
+    const styles = useStyles();
+    const globalStyles = useGlobalStyles();
 
     const { setTokenData } = useContext(TokenContext);
     const { setCourse } = useContext(CourseContext);
@@ -17,34 +43,30 @@ export function Wrapper() {
 
     return (
         <>
-            <header>
-                <h1>Calendar Exporter<sup>BETA</sup></h1>
-                <nav>
-                    <Link to="/">
-                        <img src="Home.svg" alt="Home.svg"></img>
-                        <span>Home</span>
-                    </Link>
-                    <Link to="/classroom">
-                        <img src="School.svg" alt="School.svg"></img>
-                        <span>Aule</span>
-                    </Link>
-                    <Link to="/calendar">
-                        <img src="Calendar.svg" alt="Calendar.svg"></img>
-                        <span>Calendario</span>
-                    </Link>
-                    <Link to="/about">
-                        <img src="Info.svg" alt="Info.svg"></img>
-                        <span>About</span>
-                    </Link>
-                    <Button appearance="primary" onClick={logout}>👋 Logout</Button>
+            <header className={styles.header}>
+                <h1 className={styles.title}>Calendar Exporter<sup>BETA</sup></h1>
+                <nav className={styles.nav}>
+                    <CompoundButton icon={<Home20Filled />} appearance="subtle" size="small" as="a" href="/">
+                        Home
+                    </CompoundButton>
+                    <CompoundButton icon={<ConferenceRoom20Filled />} appearance="subtle" size="small" as="a" href="/classroom">
+                        Aule
+                    </CompoundButton>
+                    <CompoundButton icon={<Calendar20Filled />} appearance="subtle" size="small" as="a" href="/calendar">
+                        Calendario
+                    </CompoundButton>
+                    <CompoundButton icon={<Info20Filled />} appearance="subtle" size="small" as="a" href="/about">
+                        About
+                    </CompoundButton>
                 </nav>
+                <Button appearance="primary" onClick={logout}>👋 Logout</Button>
             </header>
-            <main>
+            <main className={globalStyles.main}>
                 <Outlet />
             </main>
-            <footer>
-                <span>Questo progetto non è sponsorizzato e/o approvato da Fondazione JobsAcademy.</span>
-                <Link to="/about">About</Link>
+            <footer className={globalStyles.footer}>
+                <Label>Questo progetto non è sponsorizzato e/o approvato da Fondazione JobsAcademy. </Label>
+                <Link href="/about">About</Link>
             </footer>
         </>
     );
