a749cc1bd4456e2e76108ecb892daafe7221a884
Manuel Doneda <manuel.doneda@gmail.com>
2023-10-03 20:31:15 +0200

    feat: :lipstick: Update about page to fluent ui 9

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,19 +1,37 @@
+import { Body1, Card, CardHeader, Link, Subtitle1, Title2, mergeClasses } from "@fluentui/react-components";
+import { useGlobalStyles } from "../globalStyles";
+
 export function About() {
+    const globalStyles = useGlobalStyles();
+
     return (
-        <div className="main-container container align-left">
-            <div className="container align-left wide">
-                <h1>🧐 About</h1>
-            </div>
-
-            <div className="container align-left">
-                <h2>Perché?</h2>
-                <p>Il progetto è nato perché il gestionale è macchinoso e lento! Sopratutto se lo si prova ad utilizzare da smartphone mentre si è in ritardo per una lezione...</p>
-
-                <h2>Come funziona?</h2>
-                <p>Estraiamo (con molta fatica) i dati del calendario dal Gestionale ufficiale, li rendiamo utilizzabili, li salviamo in una cache e li forniamo tramite il nostro backend!</p>
-
-                <h2>Posso contattare l'autore?</h2>
-                <p>Certamente! Puoi contattarmi su <a href="https://t.me/genio2003" target="_blank">Telegram</a>!</p>
+        <div className={globalStyles.container}>
+            <Card className={globalStyles.card}>
+                <CardHeader
+                    header={<Title2>🧐 Informazioni sul progetto</Title2>}
+                />
+            </Card>
+            <div className={mergeClasses(globalStyles.container, globalStyles.list)}>
+                <Card className={globalStyles.card}>
+                    <Subtitle1>Come è nato?</Subtitle1>
+                    <Body1>
+                        Il progetto nasce perché il gestionale è macchinoso e lento!
+                        <br />
+                        Soprattutto se lo si prova ad utilizzare dal cellulare mentre si è in ritardo per una lezione...
+                    </Body1>
+                </Card>
+                <Card className={globalStyles.card}>
+                    <Subtitle1>Come funziona?</Subtitle1>
+                    <Body1>
+                        Vengono estratti (con molta fatica) i dati del calendario, corsi e aule dal gestionale ufficiale,
+                        <br />
+                        dopodiché vengono convertiti in un formato standard e salvati in una cache, pronti per essere utilizzati da <Link href={window.location.toString()}>betoniera.org</Link>.
+                    </Body1>
+                </Card>
+                <Card className={globalStyles.card}>
+                    <Subtitle1>Posso contattare l'autore?</Subtitle1>
+                    <Body1>Certamente! Puoi contattarmi su <Link href="https://t.me/genio2003" target="_blank">Telegram</Link>!</Body1>
+                </Card>
             </div>
         </div>
     );
