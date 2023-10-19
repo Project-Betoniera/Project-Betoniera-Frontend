52e40e62d4c7c4b028646e984c5c25101fd02a92
Manuel Doneda <manuel.doneda@gmail.com>
2023-10-19 10:32:06 +0200

    feat: :children_crossing: Make 'remember me' in login enabled by default

--- src/routes/LoginForm.tsx
+++ src/routes/LoginForm.tsx
@@ -63,7 +63,7 @@ export function LoginForm() {
 
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-    const [remember, setRemember] = useState<boolean>(false);
+    const [remember, setRemember] = useState<boolean>(true);
 
     const login = async (e: FormEvent) => {
         e.preventDefault();
