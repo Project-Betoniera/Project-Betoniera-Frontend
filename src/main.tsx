bdc61cdb8c18d0a00853fc42709afe92c9b00ca8
Marco Benzoni <hello@qlcvea.com>
2023-10-28 09:31:55 +0200

    fix(pwa): Call registerSW() to fix app not auto-updating
    See also: https://vite-pwa-org.netlify.app/guide/auto-update.html (warning box)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import App from "./App.tsx";
 import "./main.css";
 import { TokenContextProvider } from "./context/TokenContext.tsx";
@@ -11,6 +12,8 @@ import { isBetaBuild } from "./config.ts";
 
 ensurePlausible();
 if (isBetaBuild) console.log("Beta build");
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
