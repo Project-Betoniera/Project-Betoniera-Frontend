29198b02c310d4b009a055feb431bf9890daeb3b
Manuel Doneda <manuel.doneda@gmail.com>
2023-12-04 10:13:16 +0100

    refactor: :recycle: Use requests library, refactor

--- src/routes/ClassroomViewer.tsx
+++ src/routes/ClassroomViewer.tsx
@@ -1,14 +1,12 @@
-import { Card, CardHeader, Divider, Spinner, Subtitle2, Title1, Title2, makeStyles, mergeClasses, tokens, webLightTheme } from "@fluentui/react-components";
+import { Card, CardHeader, Divider, Spinner, Subtitle2, Title1, makeStyles, mergeClasses, tokens, webLightTheme } from "@fluentui/react-components";
 import { useContext, useEffect, useState } from "react";
 import { ClassroomStatus } from "../dto/ClassroomStatus";
-import axios from "axios";
-import { apiUrl } from "../config";
-import { TokenContext } from "../context/TokenContext";
 import { useGlobalStyles } from "../globalStyles";
 import { EventDto } from "../dto/EventDto";
 import EventDetails from "../components/EventDetails";
 import { ClassroomDto } from "../dto/ClassroomDto";
 import { ThemeContext } from "../context/ThemeContext";
+import useRequests from "../libraries/requests/requests";
 
 const useLightStyles = makeStyles({
     cardFree: {
@@ -35,66 +33,37 @@ const useDarkStyles = makeStyles({
 });
 
 export function ClassroomViewer() {
-
     const globalStyles = useGlobalStyles();
     const { theme } = useContext(ThemeContext);
     const themeStyles = theme === webLightTheme ? useLightStyles() : useDarkStyles();
 
-    const { tokenData } = useContext(TokenContext);
+    const requests = useRequests();
 
-    const [now] = useState(new Date("2023-12-04T13:55:00"));
+    const [now] = useState(new Date());
     const [classrooms, setClassrooms] = useState<ClassroomStatus[] | null>(null);
     const [currentFloor, setCurrentFloor] = useState(1);
 
     useEffect(() => {
         // First data fetch
-        fetchClassroomsStatus();
+        getClassroomsStatus();
 
         // Fetch classrooms status every 10 seconds
         const interval = setInterval(() => {
             setCurrentFloor((oldValue) => {
-                if (oldValue === 3) fetchClassroomsStatus(); // Refresh when all data has been shown
-                return oldValue === 3 ? 1 : oldValue + 1
+                if (oldValue === 3) getClassroomsStatus(); // Refresh when all data has been shown
+                return oldValue === 3 ? 1 : oldValue + 1;
             });
         }, 15000);
 
         return () => clearInterval(interval);
     }, []);
 
-    const fetchClassroomsStatus = () => {
-        console.log("Fetching classrooms status...");
+    const getClassroomsStatus = () => {
         setClassrooms(null);
-        axios.get(new URL(`classroom/status`, apiUrl).toString(), {
-            headers: { Authorization: "Bearer " + tokenData.token },
-            params: { time: now.toISOString() }
-        }).then(response => {
-            let result: ClassroomStatus[] = response.data;
 
-            const exclude = [5, 19, 26, 31, 33];
-            result = result.filter((item) => !exclude.includes(item.classroom.id));
-
-            result = result.map((item) => {
-
-                if (item.status.statusChangeAt) {
-
-                    // Convert date string to date object for statusChangeAt
-                    item.status.statusChangeAt = new Date(item.status.statusChangeAt);
-
-                    if (item.status.currentOrNextEvent) {
-                        // Convert date strings to date objects for currentOrNextEvent
-                        item.status.currentOrNextEvent.start = new Date(item.status.currentOrNextEvent.start);
-                        item.status.currentOrNextEvent.end = new Date(item.status.currentOrNextEvent.end);
-                    } else { item.status.currentOrNextEvent = null; }
-
-                } else { item.status.statusChangeAt = null; }
-
-                return item;
-            });
-
-            setClassrooms(result);
-        }).catch((error) => {
-            console.log(error);
-        });
+        requests.classroom.status(now)
+            .then(setClassrooms)
+            .catch(console.error);
     };
 
     useEffect(() => {
@@ -111,7 +80,6 @@ export function ClassroomViewer() {
     }, []);
 
     const renderFloor = (floor: number, classrooms: ClassroomStatus[]) => {
-
         const filtered = classrooms.filter((item) => item.classroom.name[0] === floor.toString());
 
         return (
@@ -129,7 +97,6 @@ export function ClassroomViewer() {
     };
 
     const renderClassrooms = (filtered: ClassroomStatus[]) => {
-
         const getBackgroundColor = (item: ClassroomStatus) => {
             if (!item.status.isFree) {
                 return themeStyles.cardBusy;
@@ -145,13 +112,12 @@ export function ClassroomViewer() {
         };
 
         return filtered.map((item) => {
-
             const getDividerText = () => {
                 if (!item.status.statusChangeAt || !item.status.currentOrNextEvent)
                     return "Libera";
                 else if (item.status.statusChangeAt.getDate() == now.getDate())
                     return `${item.status.isFree ? "Libera" : "Occupata"} fino alle ${item.status.statusChangeAt.toLocaleTimeString([], { timeStyle: "short" })}`;
-            }
+            };
 
             return (
                 <Card key={item.classroom.id} className={mergeClasses(globalStyles.card, getBackgroundColor(item))}>
@@ -165,7 +131,6 @@ export function ClassroomViewer() {
                 </Card>
             );
         });
-
     };
 
     const renderEvent = (event: Omit<EventDto, "classroom"> | null, classroom: ClassroomDto) => {
@@ -185,16 +150,15 @@ export function ClassroomViewer() {
         };
 
         if (!event || event.start.getDate() !== now.getDate() || event.end < now) {
-            return <Title2>Nessuna Lezione</Title2>
+            return <Subtitle2>Nessuna Lezione</Subtitle2>;
         }
 
-        return <EventDetails event={fixEvent(event, classroom)} titleType={"custom"} titleSize="large" subtitle={event.course.name} hide={["classroom", "course"]} title={event.course.code} />
+        return <EventDetails event={fixEvent(event, classroom)} titleType={"custom"} titleSize="large" subtitle={event.course.name} hide={["classroom", "course"]} title={event.course.code} />;
     };
 
     return (
         <div className={globalStyles.container}>
             {classrooms ? renderFloor(currentFloor, classrooms) : <Spinner label="Caricamento..." />}
         </div>
-    )
-
+    );
 }
